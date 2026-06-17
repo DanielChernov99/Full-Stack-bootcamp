f9c45d5e088f5b2309e06dfd5b5e5be16ff40443
Daniel Chernov <chernov.dani@gmail.com>
2026-06-17 15:32:57 +0300

    done q3

--- JS-Dev/Async Await/q3.js
+++ JS-Dev/Async Await/q3.js
@@ -1,32 +1,49 @@
-// All users: https://jsonplaceholder.typicode.com/users
-// All posts: https://jsonplaceholder.typicode.com/posts
-// All comments: https://jsonplaceholder.typicode.com/comments
-
-
-const fetchAllAndAggregate = async function(){
+const fetchAllAndAggregate = async function () {
     const [users, posts, comments] = await Promise.all([
-        fetch(`https://jsonplaceholder.typicode.com/users`).then((res) => res.json()),
+        fetch(`https://jsonplaceholder.typicode.com/users`).then(res => res.json()),
         fetch(`https://jsonplaceholder.typicode.com/posts`).then(res => res.json()),
         fetch(`https://jsonplaceholder.typicode.com/comments`).then(res => res.json())
     ])
+
     const summary = {
         totalUsers: users.length,
         totalPosts: posts.length,
         totalComments: comments.length,
-        avgPostsPerUser: totalPosts / totalUsers,
-        avgCommentsPerPost: totalComments / totalPosts
+        avgPostsPerUser: posts.length / users.length,
+        avgCommentsPerPost: comments.length / posts.length
     }
 
-    const topUsers = {
-        
+    const topUsers = []
+
+    for (const user of users) {
+        const userPosts = posts.filter(post => post.userId === user.id)
+
+        let commentCount = 0
+
+        for (const post of userPosts) {
+            comments.forEach(comment => {
+                if (comment.postId === post.id) {
+                    commentCount++
+                }
+            })
+        }
+
+        topUsers.push({
+            name: user.name,
+            postCount: userPosts.length,
+            commentCount: commentCount
+        })
     }
 
+    topUsers.sort((a, b) => b.postCount - a.postCount)
 
+    const recentPosts = posts
+        .sort((a, b) => b.id - a.id)
+        .slice(0, 5)
 
-
-    return{
-        summary:summary,
-        topUsers:,
-        recentPosts:
+    return {
+        summary: summary,
+        topUsers: topUsers.slice(0, 3),
+        recentPosts: recentPosts
     }
 }
