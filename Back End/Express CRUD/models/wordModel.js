03604e08fc1640d2d6e8f829047c06d883ac15bb
Daniel Chernov <chernov.dani@gmail.com>
2026-07-27 14:33:00 +0300

    feat: refactor word handling functions to use normalizeWord for consistency

--- Back End/Express CRUD/models/wordModel.js
+++ Back End/Express CRUD/models/wordModel.js
@@ -1,23 +1,46 @@
 const wordCounter = {};
 
+function normalizeWord(word) {
+  return word.toLowerCase().replace(/[^a-z]/g, "");
+}
+
 function getWordCount(word) {
-  const cleanWord = word.toLowerCase();
+  const cleanWord = normalizeWord(word);
+
+  if (!cleanWord) {
+    return 0;
+  }
+
   return wordCounter[cleanWord] || 0;
 }
 
 function addWord(word) {
-  const cleanWord = word.toLowerCase();
+  const cleanWord = normalizeWord(word);
+
+  if (!cleanWord) {
+    return null;
+  }
+
   wordCounter[cleanWord] = (wordCounter[cleanWord] || 0) + 1;
+
   return wordCounter[cleanWord];
 }
 
 function addSentence(sentence) {
   let numNewWords = 0;
   let numOldWords = 0;
-  const words = sentence.toLowerCase().split(" ");
+
+  const words = sentence.trim().split(/\s+/).map(normalizeWord).filter(Boolean);
+
   for (const word of words) {
     const countBeforeAdding = getWordCount(word);
-    countBeforeAdding === 0 ? numNewWords++ : numOldWords++;
+
+    if (countBeforeAdding === 0) {
+      numNewWords++;
+    } else {
+      numOldWords++;
+    }
+
     addWord(word);
   }
 
@@ -28,13 +51,14 @@ function addSentence(sentence) {
 }
 
 function deleteWord(word) {
-  const cleanWord = word.toLowerCase();
+  const cleanWord = normalizeWord(word);
 
-  if (!Object.hasOwn(wordCounter, cleanWord)) {
+  if (!cleanWord || !Object.hasOwn(wordCounter, cleanWord)) {
     return false;
   }
 
   delete wordCounter[cleanWord];
+
   return true;
 }
 
