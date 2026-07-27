78b83c7e100ea13958fddea91491d99c8ff89077
Daniel Chernov <chernov.dani@gmail.com>
2026-07-27 14:30:48 +0300

    feat: enhance word model and routes with addSentence and deleteWord functionalities

--- Back End/Express CRUD/models/wordModel.js
+++ Back End/Express CRUD/models/wordModel.js
@@ -1,9 +1,46 @@
-const wordCounter = { cat: 1 };
+const wordCounter = {};
 
 function getWordCount(word) {
-  return wordCounter[word] || 0;
+  const cleanWord = word.toLowerCase();
+  return wordCounter[cleanWord] || 0;
+}
+
+function addWord(word) {
+  const cleanWord = word.toLowerCase();
+  wordCounter[cleanWord] = (wordCounter[cleanWord] || 0) + 1;
+  return wordCounter[cleanWord];
+}
+
+function addSentence(sentence) {
+  let numNewWords = 0;
+  let numOldWords = 0;
+  const words = sentence.toLowerCase().split(" ");
+  for (const word of words) {
+    const countBeforeAdding = getWordCount(word);
+    countBeforeAdding === 0 ? numNewWords++ : numOldWords++;
+    addWord(word);
+  }
+
+  return {
+    numNewWords,
+    numOldWords,
+  };
+}
+
+function deleteWord(word) {
+  const cleanWord = word.toLowerCase();
+
+  if (!Object.hasOwn(wordCounter, cleanWord)) {
+    return false;
+  }
+
+  delete wordCounter[cleanWord];
+  return true;
 }
 
 module.exports = {
   getWordCount,
+  addWord,
+  addSentence,
+  deleteWord,
 };
--- Back End/Express CRUD/routes/wordRoutes.js
+++ Back End/Express CRUD/routes/wordRoutes.js
@@ -17,10 +17,48 @@ router.get("/word/:word", function (req, res) {
 });
 
 // Exercise 3
+router.post("/word", function (req, res) {
+  const word = req.body.word;
+  if (!word) {
+    return res.status(400).json({ error: "bad request " });
+  }
+  const wordCount = wordModel.addWord(word);
+  res.status(201).json({
+    text: `Added ${word}`,
+    currentCount: wordCount,
+  });
+});
 
 // Exercise 4
+router.post("/sentence", function (req, res) {
+  const sentence = req.body.sentence;
+  if (!sentence) {
+    return res.status(400).json({
+      error: "Sentence is required",
+    });
+  }
+  const result = wordModel.addSentence(sentence);
+  res.status(201).json({
+    text: `Added ${result.numNewWords} words, ${result.numOldWords} already existed`,
+    currentCount: -1,
+  });
+});
 
 // Exercise 5
+router.delete("/word/:word", function (req, res) {
+  const word = req.params.word;
+  const wasDeleted = wordModel.deleteWord(word);
+
+  if (!wasDeleted) {
+    return res.status(404).json({
+      error: `Word "${word}" was not found`,
+    });
+  }
+
+  res.status(200).json({
+    message: `Word "${word}" was deleted successfully`,
+  });
+});
 
 // Extension 1
 
